86045c6060aea55271c5188dfc4a53598211f868
lunyamwis <lutherlunyamwi@gmail.com>
2025-12-16 14:00:06 +0300

    adding

--- emr/admin.py
+++ emr/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import LabResult, LabTest, Lab, LabQueue
 # Register your models here.
 
-admin.site.register(LabTest)
+
+
+@admin.register(LabTest)
+class LabTestAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "unit", "price", )
+    list_filter = ("created_at")
+    search_fields = ("name",)
 admin.site.register(LabResult)
 admin.site.register(Lab)
 admin.site.register(LabQueue)
