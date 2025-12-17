a5639a750e0f74a894ec5256105fd0fa0e6ef775
lunyamwis <lutherlunyamwi@gmail.com>
2025-12-17 11:16:09 +0300

    usig daphne

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,10 @@ celery -A setup beat --loglevel=info &
 
 sleep 5
 
-echo "<<<<<<<<<<<<<<<<<<<< START setup >>>>>>>>>>>>>>>>>>>>>>>>"
+#echo "<<<<<<<<<<<<<<<<<<<< START setup >>>>>>>>>>>>>>>>>>>>>>>>"
 # python manage.py runserver
 # Start the setup with gunicorn
-gunicorn --bind 0.0.0.0:8000 setup.wsgi --reload --access-logfile '-' --workers=2
+#gunicorn --bind 0.0.0.0:8000 setup.wsgi --reload --access-logfile '-' --workers=2
+
+echo "<<<<<<<< Start Daphne (ASGI) >>>>>>>>"
+daphne -b 0.0.0.0 -p 8000 setup.asgi:application
